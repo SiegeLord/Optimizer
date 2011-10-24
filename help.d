304ea7e45ad5cb14fdefdaa0a2bc7817dc7e4a77
SiegeLord <slabode@aim.com>
2011-10-24 17:09:04 -0400

    Add license

--- help.d
+++ help.d
@@ -1,7 +1,25 @@
+/*
+Optimizer, a command line function minimization software.
+Copyright (C) 2011  Pavel Sountsov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 module help;
 
 const char[] Help = 
-`Optimizer, a command line function minimization software.
+`Optimizer, a command line function minimization software. Copyright (C) 2011 Pavel Sountsov.
 
 Usage: optimizer [OPTION]... [ALGORITHM_OPTION]... [--] COMMAND [COMMAND_OPTIONS]...
 Example: optimizer -l=-1:1 -l=-1:1 -a de -v -- ./test --test_arg
